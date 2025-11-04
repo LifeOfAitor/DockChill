f2760ec7ce0de34e388501584aa502ca32459a48
Aitor <aitorgaillard99@gmail.com>
2025-11-04 07:50:36 +0100

    start implementing the fragments, need to fix databinding feature somehow

--- app/src/main/java/com/dev/dockchill/MainActivity.kt
+++ app/src/main/java/com/dev/dockchill/MainActivity.kt
@@ -3,18 +3,27 @@ package com.dev.dockchill
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
+import com.dev.dockchill.databinding.ActivityMainBinding
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.pager.adapter = ScreenSlidePagerAdapter(this)
+
+        TabLayoutMediator(binding.tabLayout, binding.pager) { tab, position ->
+            when (position) {
+                0 -> tab.text = "Passwords"
+                else -> tab.text = "General"
+            }
+        }.attach()
     }
 }
