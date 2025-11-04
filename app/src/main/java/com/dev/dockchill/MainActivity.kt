5a73c6d2a6aa98b73fb9c30c478f56904137177f
aitorzubiri <aitorgaillard99@gmail.com>
2025-11-04 10:31:33 +0100

    set orientation to landscape and make fragments work in fullscreen

--- app/src/main/java/com/dev/dockchill/MainActivity.kt
+++ app/src/main/java/com/dev/dockchill/MainActivity.kt
@@ -3,27 +3,39 @@ package com.dev.dockchill
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.dev.dockchill.databinding.ActivityMainBinding
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.dev.dockchill.databinding.ActivityMainBinding
 import com.google.android.material.tabs.TabLayoutMediator
+
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.pager.adapter = ScreenSlidePagerAdapter(this)
 
-        TabLayoutMediator(binding.tabLayout, binding.pager) { tab, position ->
-            when (position) {
-                0 -> tab.text = "Passwords"
-                else -> tab.text = "General"
-            }
+        TabLayoutMediator(binding.tabLayout, binding.pager) { _, _ ->
         }.attach()
+
+    }
+
+    private inner class ScreenSlidePagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+        override fun getItemCount(): Int = 3
+
+        override fun createFragment(position: Int): Fragment {
+            return when (position) {
+                0 -> pomodoroFragment()
+                1 -> mainScreenFragment()
+                else -> todo_listFragment()
+            }
+        }
     }
 }
