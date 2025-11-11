f7c8151e13464ffa5291dd8ba41c6b7c93801c82
aitorzubiri <aitorgaillard99@gmail.com>
2025-11-11 13:09:48 +0100

    hobekuntzak egin pantaia osoan ondo ikusteko eta layout-a ezarri main fragmentarentzako

--- app/src/main/java/com/dev/dockchill/MainActivity.kt
+++ app/src/main/java/com/dev/dockchill/MainActivity.kt
@@ -3,17 +3,19 @@ package com.dev.dockchill
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.dev.dockchill.databinding.ActivityMainBinding
-import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Ertzetaraino marrazteko gaitasuna aktibatu
         enableEdgeToEdge()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -21,12 +23,38 @@ class MainActivity : AppCompatActivity() {
 
         binding.pager.adapter = ScreenSlidePagerAdapter(this)
 
-        TabLayoutMediator(binding.tabLayout, binding.pager) { _, _ ->
-        }.attach()
-
-        // kargatuko du defektuz "1" posizioan dagoen fragmentua
+        // Defektuz "1" posizioan dagoen fragmentua kargatuko du
         binding.pager.currentItem = 1
 
+        // **MODU IMMERSIBOA AKTIBATU:** // Botoiak eta egoera barra ezkutatu, irristatuz agertuko dira (swipe).
+        enterImmersiveMode()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // Garrantzitsua da deitzea onResume-n, erabiltzaileak aplikaziora itzultzean
+        // ezkutuko egoera mantentzeko.
+        enterImmersiveMode()
+    }
+
+    /**
+     * Modu inmersiboa aktibatzen du. Sistemako barra guztiak ezkutatzen ditu
+     * eta irristatuz (swipe) berriz agertzea ahalbidetzen du, baina ezkutuan mantentzen da.
+     */
+    private fun enterImmersiveMode() {
+        // Leihoaren eragina kudeatzeko kontrolagailua lortu
+        val controller = WindowInsetsControllerCompat(window, window.decorView)
+
+        // 1. Nabigazio-barra (Home, Atzera, Multitarea botoiak) ezkutatu
+        controller.hide(WindowInsetsCompat.Type.navigationBars())
+
+        // 2. Egoera-barra (Ordua, Jakinarazpenak) ezkutatu
+        controller.hide(WindowInsetsCompat.Type.statusBars())
+
+        // 3. Jokabidea konfiguratu:
+        // Irristatze labur batekin (swipe) agertu eta gero berriro ezkutatu
+        controller.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
     }
 
     // fragmentak slider bezala funtzionatu dezaten eta fragmentak sortzeko gure MainActivity honetan
@@ -36,9 +64,9 @@ class MainActivity : AppCompatActivity() {
         //fragmentak sortu eta ezarri
         override fun createFragment(position: Int): Fragment {
             return when (position) {
-                0 -> pomodoroFragment()
-                1 -> MainScreenFragment()
-                else -> todo_listFragment()
+                0 -> pomodoroFragment() // Fragmentua 0. posizioan
+                1 -> MainScreenFragment() // Fragmentua 1. posizioan (Defektuzkoa)
+                else -> todo_listFragment() // Fragmentua 2. posizioan
             }
         }
     }
